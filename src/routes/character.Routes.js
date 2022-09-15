725e563c307e114c4d59554c54e6aa330d17e18f
José Miguel Visa <josemiguel_18113@outlook.es>
2022-09-14 22:19:40 -0300

    Using 'validateCreateCharacter' middleware

--- src/routes/character.Routes.js
+++ src/routes/character.Routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
+const { validateCreateCharacters } = require("../middlewares");
 const { createCharacter, deleteCharacter, getCharacters, getCharacterByid, updateCharacter } = require ("../controllers/characters")
 
 const routes = Router();
 
-routes.post("/characters", createCharacter);
+routes.post("/characters", validateCreateCharacters, createCharacter)
 routes.put("/characters/:id", updateCharacter);
 routes.delete("/characters/:id", deleteCharacter);
 routes.get("/characters", getCharacters);
